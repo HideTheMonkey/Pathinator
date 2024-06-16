d41737342d40dde2da8bab4e600ba5755005bae7
HideTheMonkey <github@hidefamily.net>
2024-06-16 16:46:53 -0500

    Move common command logic into abstract parent class.

--- src/main/java/com/hidethemonkey/pathinator/commands/PathCommands.java
+++ src/main/java/com/hidethemonkey/pathinator/commands/PathCommands.java
@@ -24,6 +24,21 @@
 
 package com.hidethemonkey.pathinator.commands;
 
+import dev.jorel.commandapi.executors.CommandArguments;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.ItemStack;
+
+import com.hidethemonkey.pathinator.Pathinator;
+import com.hidethemonkey.pathinator.PathinatorConfig;
+import com.hidethemonkey.pathinator.helpers.BlockHelper;
+import com.hidethemonkey.pathinator.helpers.PlayerHelper;
+
 public abstract class PathCommands {
 
     // primary commands
@@ -35,5 +50,72 @@ public abstract class PathCommands {
     public static final String DISTANCE = "distance";
     public static final String WITH_LIGHTS = "with lights";
     public static final String WITH_POWER = "with power";
+
+    protected Pathinator plugin;
+    protected PathinatorConfig config;
+
+    public PathCommands(Pathinator pathPlugin) {
+        this.plugin = pathPlugin;
+        this.config = pathPlugin.getPConfig();
+    }
+
+    protected Integer getDistance(CommandArguments args) {
+        return (Integer) args.getOrDefault(DISTANCE, 0);
+    }
+
+    protected boolean getWithLights(CommandArguments args) {
+        return (boolean) args.getOrDefault(WITH_LIGHTS, false);
+    }
+
+    protected boolean getWithPower(CommandArguments args) {
+        return (boolean) args.getOrDefault(WITH_POWER, false);
+    }
+
+    protected boolean modeCheck(PlayerHelper playerHelper) {
+        // Check if the player is in the correct game mode
+        if (playerHelper.isInAdventure() || playerHelper.isInSpectator()) {
+            playerHelper.msg("Pathinator does not work in " + playerHelper.getGameMode() + " mode.");
+            return false;
+        }
+        // Check if we're enabled in survival mode
+        if (playerHelper.isInSurvival() && !config.getEnabledInSurvival()) {
+            playerHelper.msg("Pathinator is disabled in survival mode.");
+            return false;
+        }
+        return true;
+    }
+
+    protected Block findTargetBlock(BlockHelper blockHelper, PlayerHelper playerHelper) {
+        Block block = blockHelper.getBlockUnderPlayer(playerHelper.getPlayer());
+        Material blockMaterial = block.getBlockData().getMaterial();
+        if (blockMaterial == Material.AIR && playerHelper.isInSurvival()) {
+            playerHelper.msg("Found a block of AIR. Please stand on a solid block to place a path.");
+            return null;
+        }
+        return block;
+    }
+
+    protected ArrayList<ItemStack> getLightingStack(CommandArguments args, PlayerHelper playerHelper) {
+        ArrayList<ItemStack> lightingStack = new ArrayList<ItemStack>();
+        if (getWithLights(args)) {
+            int clearance = config.getClearance();
+            List<String> configuredStack = config.getLightingStack();
+            for (int i = 0; i < configuredStack.size(); i++) {
+                // Don't allow the lighting stack to exceed the clearance height in survival
+                if (playerHelper.isInSurvival()) {
+                    --clearance;
+                }
+                if (clearance >= 0) {
+                    Material material = Material.getMaterial(configuredStack.get(i));
+                    if (material != null) {
+                        lightingStack.add(new ItemStack(material));
+                    }
+                }
+            }
+        }
+        return lightingStack;
+    }
+
+    public abstract void createPath(CommandSender sender, CommandArguments args);
 
 }
--- src/main/java/com/hidethemonkey/pathinator/commands/TrackCommands.java
+++ src/main/java/com/hidethemonkey/pathinator/commands/TrackCommands.java
@@ -27,7 +27,6 @@ package com.hidethemonkey.pathinator.commands;
 import dev.jorel.commandapi.executors.CommandArguments;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -38,19 +37,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import com.hidethemonkey.pathinator.Pathinator;
-import com.hidethemonkey.pathinator.PathinatorConfig;
 import com.hidethemonkey.pathinator.helpers.BlockHelper;
 import com.hidethemonkey.pathinator.helpers.PlayerHelper;
 import com.hidethemonkey.pathinator.helpers.SegmentData;
 
-public class TrackCommands {
-
-    private Pathinator plugin;
-    private PathinatorConfig config;
+public class TrackCommands extends PathCommands {
 
     public TrackCommands(Pathinator pathPlugin) {
-        this.plugin = pathPlugin;
-        this.config = pathPlugin.getPConfig();
+        super(pathPlugin);
     }
 
     /**
@@ -58,40 +52,34 @@ public class TrackCommands {
      * @param sender
      * @param args
      */
-    public void tracksPath(CommandSender sender, CommandArguments args) {
+    @Override
+    public void createPath(CommandSender sender, CommandArguments args) {
         Player player = (Player) sender;
         PlayerHelper playerHelper = new PlayerHelper(player, plugin);
 
-        // Check if the player is in the correct game mode
-        if (playerHelper.isInAdventure() || playerHelper.isInSpectator()) {
-            playerHelper.msg("Pathinator does not work in " + playerHelper.getGameMode() + " mode.");
-            return;
-        }
-        // Check if we're enabled in survival mode
-        if (playerHelper.isInSurvival() && !config.getEnabledInSurvival()) {
-            playerHelper.msg("Pathinator is disabled in survival mode.");
+        // Check if the player is in a supported game mode
+        if (!modeCheck(playerHelper)) {
             return;
         }
 
         BlockHelper blockHelper = new BlockHelper(plugin);
 
-        // Find the block under the player
-        Block block = blockHelper.getBlockUnderPlayer(player);
-        int inventoryCount = playerHelper.getItemCount(block);
-        Material blockMaterial = block.getBlockData().getMaterial();
-        if (blockMaterial == Material.AIR && playerHelper.isInSurvival()) {
-            playerHelper.msg("Standing on AIR. Please stand on a solid block to place a path.");
+        // Get the block under the player
+        Block targetBlock = findTargetBlock(blockHelper, playerHelper);
+        if (targetBlock == null) {
             return;
         }
 
-        Location blockLocation = block.getLocation();
+        int inventoryCount = playerHelper.getItemCount(targetBlock);
 
         BlockFace facing = player.getFacing();
 
-        Integer blockCount = (Integer) args.getOrDefault(PathCommands.DISTANCE, 0);
+        Integer blockCount = getDistance(args);
         if (blockCount <= 0) {
             return;
         }
+
+        // copy the original count
         int requestedCount = blockCount;
 
         // Don't allow more blocks to be placed than are in the player's inventory
@@ -99,63 +87,44 @@ public class TrackCommands {
             blockCount = inventoryCount;
         }
 
-        Boolean withLights = args.getByClass(PathCommands.WITH_LIGHTS, Boolean.class);
-        withLights = withLights != null ? withLights : false;
+        final ArrayList<ItemStack> lightingStack = getLightingStack(args, playerHelper);
 
-        ArrayList<ItemStack> lightingStacks = new ArrayList<ItemStack>();
-        if (withLights) {
-            int clearance = config.getClearance();
-            List<String> configuredStack = config.getLightingStack();
-            for (int i = 0; i < configuredStack.size(); i++) {
-                // Don't allow the lighting stack to exceed the clearance height in survival
-                if (playerHelper.isInSurvival()) {
-                    --clearance;
-                }
-                if (clearance >= 0) {
-                    lightingStacks.add(new ItemStack(Material.getMaterial(configuredStack.get(i))));
-                }
-            }
-        }
-
-        int lightingInterval = config.getLightingInterval();
-        Location placedLocation = blockLocation.clone();
-
-        Boolean withPower = args.getByClass(PathCommands.WITH_POWER, Boolean.class);
-        withPower = withPower != null ? withPower : false;
-        int poweredInterval = config.getPoweredInterval();
+        Location placedLocation = targetBlock.getLocation().clone();
 
         for (int i = 0; i < blockCount; i++) {
             blockHelper.adjustLocationForward(placedLocation, facing);
 
             SegmentData segmentData = new SegmentData();
-            segmentData.setBaseFacing(facing);
-            segmentData.setBaseMaterial(blockMaterial);
             segmentData.setWorld(player.getWorld());
+            segmentData.setBaseFacing(facing);
+            segmentData.setBaseMaterial(targetBlock.getBlockData().getMaterial());
+            segmentData.setBaseLocation(placedLocation);
             segmentData.setClearance(config.getClearance());
             segmentData.setClearanceMaterial(Material.getMaterial(config.getClearanceMaterial()));
-            segmentData.setBaseLocation(placedLocation);
             segmentData.setUseRails(true);
 
-            if (withPower && i != 0 && i % poweredInterval == 0) {
+            if (getWithPower(args) && i != 0 && (i % config.getPoweredInterval()) == 0) {
                 segmentData.addPower();
             }
 
-            if (withLights && i != 0 && i % lightingInterval == 0) {
-                segmentData.addLightingStacks(lightingStacks);
+            if (getWithLights(args) && i != 0 && (i % config.getLightingInterval()) == 0) {
+                segmentData.addLightingStacks(lightingStack);
                 segmentData.addLighting();
             }
 
             // This is where the magic happens
             blockHelper.placeBlock(segmentData, i, playerHelper);
         }
+
         if (requestedCount != blockCount) {
             playerHelper.msg(
-                    "Requested " + requestedCount + " blocks of " + block.getType().name() + ", but only able to place "
+                    "Requested " + requestedCount + " blocks of " + targetBlock.getType().name()
+                            + ", but only able to place "
                             + blockCount + ".");
         } else {
             String prefix = playerHelper.isInSurvival() ? "Attempting to place " : "Placed ";
-            String suffix = withPower ? " with powered RAILS." : " with RAILS.";
-            playerHelper.msg(prefix + blockCount + " blocks of " + block.getType().name() + suffix);
+            String suffix = getWithPower(args) ? " with powered RAILS." : " with RAILS.";
+            playerHelper.msg(prefix + blockCount + " blocks of " + targetBlock.getType().name() + suffix);
         }
     }
 }
